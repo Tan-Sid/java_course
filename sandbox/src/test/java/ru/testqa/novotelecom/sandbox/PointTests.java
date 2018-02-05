ac8e68b2e5d110261f537ca641fca3bbe1574a45
Tan-Sid <Tanyusha_88@list.ru>
2018-02-05 15:46:18 +0700

    Правки по тестам

--- sandbox/src/test/java/ru/testqa/novotelecom/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/testqa/novotelecom/sandbox/PointTests.java
@@ -7,16 +7,23 @@ public class PointTests {
 
   @Test
   public void Distance() {
-    Point p1 = new Point(3,4);
-    Point p2 = new Point(5,4);
-    //assert p1.distance(p1,p2) == 2.0;
-    Assert.assertEquals(p1.distance(p2),2.0);
-    Assert.assertEquals(p1.distance(p2),2);
-    Assert.assertEquals(p1.distance(p2),25.0);
-    Assert.assertEquals(p1.distance(p2),-2.0);
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(5, 4);
+    Assert.assertEquals(p1.distance(p2), 2.0);
+  }
 
+  @Test
+  public void Distance2() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), 0.0);
+  }
 
-
+  @Test
+  public void Distance1() {
+    Point p1 = new Point(-3, -4);
+    Point p2 = new Point(-5, -4);
+    Assert.assertEquals(p1.distance(p2)>0, "Расстояние не может быть < 0");
   }
 
 }
