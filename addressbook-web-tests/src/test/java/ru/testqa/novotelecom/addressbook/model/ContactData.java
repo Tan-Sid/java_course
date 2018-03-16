c1904fc5b668a259614286ae83f9b6dd81be96ed
Tan-Sid <Tanyusha_88@list.ru>
2018-03-16 10:44:30 +0700

    Извлечение данных из БД для Contact

--- addressbook-web-tests/src/test/java/ru/testqa/novotelecom/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/testqa/novotelecom/addressbook/model/ContactData.java
@@ -2,34 +2,79 @@ package ru.testqa.novotelecom.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.beans.Transient;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
+  @Column(name = "firstname")
   private String firstname;
+
+  @Column(name = "lastname")
   private String lastname;
+
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
+
+  @Column(name = "home")
+  @Type(type = "text")
   private String homephone;
+
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilephone;
+
+  @Column(name = "work")
+  @Type(type = "text")
   private String workphone;
+
+  @Column(name = "email")
+  @Type(type = "text")
   private String email1;
+
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email2;
+
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email3;
+
+  @javax.persistence.Transient
   private String allPhones;
+
+  @javax.persistence.Transient
   private String allEmails;
+
+  @javax.persistence.Transient
   private String group;
-  private File photo;
+
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
