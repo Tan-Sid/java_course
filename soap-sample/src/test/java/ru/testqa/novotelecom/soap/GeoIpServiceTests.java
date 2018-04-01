61ffaacd306863906ac0fba7d4c614b34a0d5673
Tan-Sid <Tanyusha_88@list.ru>
2018-04-01 21:45:03 +0700

    Soap-простой пример, НО GeoIp возвращает 404

---
+++ soap-sample/src/test/java/ru/testqa/novotelecom/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.testqa.novotelecom.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("178.49.147.43");
+    assertEquals(geoIP.getCountryCode(), "RUS");
+  }
+}
